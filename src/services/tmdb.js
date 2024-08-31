a998c7eed1e3d3fa7f6da8693227a49cd23efd3f
Daniel <danielschopf04@gmail.com>
2024-08-31 19:53:00 -0300

    CommitFinal

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração base da API TMDB
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'fa8ea556177c0c06e3a8b381241ea5ca'; // Armazene a chave da API em uma variável de ambiente
+const API_KEY = process.env.TMDB_KEY; // Armazene a chave da API em uma variável de ambiente
 
 // Instância do axios configurada para o TMDB
 const tmdbAPI = axios.create({
